45301fd153d9fa4f2b7b241089ef0b418bf5b62b
Christoph Krey <krey.christoph@gmail.com>
2016-01-25 14:17:18 +0100

    [NEW] mass subscriptions in background thread

--- sensorimporter.py
+++ sensorimporter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__    = 'Christoph Krey <krey.christoph()gmail.com>'
+__author__	= 'Christoph Krey <krey.christoph()gmail.com>'
 __copyright__ = 'Copyright 2016 Christoph Krey'
 
 import sys
@@ -16,6 +16,7 @@ import socket
 import codecs
 from tsplit import twosplit
 import config
+import threading
 
 sys.stdout = codecs.getwriter("utf-8")(sys.__stdout__) 
 
@@ -23,128 +24,140 @@ log = logging.getLogger(__name__)
 
 cf = config.Config(os.getenv('SENSORIMPORTER', 'sensorimporter.conf'))
 
-r = cf.config('relayr')
-
-m = cf.config('mqtt')
-
-with open('XDKs.json') as data_file:    
-    xdksJson= json.load(data_file)
-    xdks = xdksJson['XDKs']
-    lut = {}
-
-def on_relayrconnect(mosq, userdata, rc):
-    global lut
-    print "connected relayr"
-    if rc != 0:
-        log.error("Can't connect to MQTT. rc=={0}".format(rc))
-        sys.exit(1)
-    for xdk in xdks:
-        xdkdict = xdks[xdk]
-        topic = xdkdict['topic']
-        print "subscribing to %s, %s" % (xdk, topic)
-	lut[xdkdict['deviceId']] = xdk
-        mosq.subscribe(str(topic), 0)
-	new_topic = "%s/%s/%s" % (m.get('prefix'), xdk, 'info')
-	new_payload = {'deviceId': xdkdict['deviceId'], 'x': xdkdict['x'], 'y': xdkdict['y'], 'z': xdkdict['z']}
-	mqttc.publish(new_topic, json.dumps(new_payload), qos=2, retain=True)
-    log.info("Connected to and subscribed to relayr MQTT broker")
-
-def on_subscribe(client, userdata, mid, granted_qos):
-	print "subscribed m=%d %d" % (mid, granted_qos[0])
-
-def on_relayrdisconnect(mosq, userdata, rc):
-    reasons = {
-       '0' : 'Connection Accepted',
-       '1' : 'Connection Refused: unacceptable protocol version',
-       '2' : 'Connection Refused: identifier rejected',
-       '3' : 'Connection Refused: server unavailable',
-       '4' : 'Connection Refused: bad user name or password',
-       '5' : 'Connection Refused: not authorized',
-    }
-    log.error("Disconnected from relayr: code={0} ({1})".format(rc, reasons.get(rc, 'unknown')))
-    print "disconnected"
-
+#########################
+# re-publish to mqtt
+#########################
 def on_connect(mosq, userdata, rc):
-    if rc != 0:
-        log.error("Can't connect to MQTT. rc=={0}".format(rc))
-        sys.exit(1)
+	if rc != 0:
+		log.error("Can't connect to MQTT. rc=={0}".format(rc))
+		sys.exit(1)
 
-    print "connected mqtt"
-    log.info("Connected to MQTT broker")
+	print "connected mqtt"
+	log.info("Connected to MQTT broker")
 
 def on_disconnect(mosq, userdata, rc):
-    reasons = {
-       '0' : 'Connection Accepted',
-       '1' : 'Connection Refused: unacceptable protocol version',
-       '2' : 'Connection Refused: identifier rejected',
-       '3' : 'Connection Refused: server unavailable',
-       '4' : 'Connection Refused: bad user name or password',
-       '5' : 'Connection Refused: not authorized',
-    }
-    log.error("Disconnected: code={0} ({1})".format(rc, reasons.get(rc, 'unknown')))
+	reasons = {
+	   '0' : 'Connection Accepted',
+	   '1' : 'Connection Refused: unacceptable protocol version',
+	   '2' : 'Connection Refused: identifier rejected',
+	   '3' : 'Connection Refused: server unavailable',
+	   '4' : 'Connection Refused: bad user name or password',
+	   '5' : 'Connection Refused: not authorized',
+	}
+	log.error("Disconnected: code={0} ({1})".format(rc, reasons.get(rc, 'unknown')))
 
-def on_message(mosq, userdata, msg):
-    if msg.retain == 1:
-        return
-
-    try:
-    	data = json.loads(str(msg.payload))
-    except:
-        return
-
-    readings = data['readings']
-    for reading in readings:
-	xdk = lut[data['deviceId']]
-	new_topic = "%s/%s/%s" % (m.get('prefix'), xdk, reading['path'])
-	new_payload = reading['value']
-	print xdk + " : " + new_topic, " = ", new_payload
-	mqttc.publish(new_topic, json.dumps(new_payload), qos=2, retain=True)
-
-clientid = r.get('client_id', 'sensorimporter-{0}'.format(os.getpid()))
-
-relayr = paho.Client(clientid, clean_session=True, userdata=None, protocol=paho.MQTTv311)
-relayr.on_message = on_message
-relayr.on_subscribe = on_subscribe
-relayr.on_connect = on_relayrconnect
-relayr.on_disconnect = on_relayrdisconnect
-
-if r.get('username') is not None:
-    print "pwset %s %s" % (r.get('username'), r.get('password'))
-    relayr.username_pw_set(r.get('username'), r.get('password'))
-
-host = r.get('host', 'localhost')
-port = int(r.get('port', 1883))
-try:
-    print "connecting to %s:%d" % (host, port)
-    relayr.connect(host, port, 60)
-    relayr.loop_start()
-except Exception, e:
-    sys.exit("Connect to `%s:%d': %s" % (host, port, str(e)))
-
+m = cf.config('mqtt')
 clientid = m.get('client_id', 'sensorimporter-{0}'.format(os.getpid()))
 mqttc = paho.Client(clientid, clean_session=True, userdata=None, protocol=paho.MQTTv311)
-mqttc.on_message = on_message
 mqttc.on_connect = on_connect
 mqttc.on_disconnect = on_disconnect
 
 if m.get('username') is not None:
-    mqttc.username_pw_set(m.get('username'), m.get('password'))
+	mqttc.username_pw_set(m.get('username'), m.get('password'))
 
 host = m.get('host', 'localhost')
 port = int(m.get('port', 1883))
 try:
-    print "connecting to %s:%d" % (host, port)
-    mqttc.connect(host, port, 60)
-    mqttc.loop_start()
+	print "connecting to %s:%d" % (host, port)
+	mqttc.loop_start()
+	mqttc.connect(host, port, 60)
 except Exception, e:
-    sys.exit("Connect to `%s:%d': %s" % (host, port, str(e)))
+	sys.exit("Connect to `%s:%d': %s" % (host, port, str(e)))
+
+
+#########################
+# subscribe to relayr
+#########################
+with open('XDKs.json') as data_file:	
+	xdksJson= json.load(data_file)
+	xdks = xdksJson['XDKs']
+	lut = {}
+
+def subscriber():
+	for xdk in xdks:
+		xdkdict = xdks[xdk]
+		topic = xdkdict['topic']
+		lut[xdkdict['deviceId']] = xdk
+		(result, mid) = relayr.subscribe(str(topic), 0)
+		print "subscribing to %s, %s: r%d,m%d" % (xdk, topic, result, mid)
+		new_topic = "%s/%s/%s" % (m.get('prefix'), xdk, 'info')
+		new_payload = {'deviceId': xdkdict['deviceId'], 'x': xdkdict['x'], 'y': xdkdict['y'], 'z': xdkdict['z']}
+		mqttc.publish(new_topic, json.dumps(new_payload), qos=2, retain=True)
+		time.sleep(1)
+	log.info("Connected to and subscribed to relayr MQTT broker")
+
+subscriberThread = threading.Thread(target=subscriber)
+
+def on_relayrconnect(mosq, userdata, rc):
+	global lut
+	print "connected relayr"
+	if rc != 0:
+		log.error("Can't connect to MQTT. rc=={0}".format(rc))
+		sys.exit(1)
+	subscriberThread.start()
+
+def on_relayrsubscribe(client, userdata, mid, granted_qos):
+	print "subscribed m=%d %d" % (mid, granted_qos[0])
+
+def on_relayrdisconnect(mosq, userdata, rc):
+	reasons = {
+	   '0' : 'Connection Accepted',
+	   '1' : 'Connection Refused: unacceptable protocol version',
+	   '2' : 'Connection Refused: identifier rejected',
+	   '3' : 'Connection Refused: server unavailable',
+	   '4' : 'Connection Refused: bad user name or password',
+	   '5' : 'Connection Refused: not authorized',
+	}
+	log.error("Disconnected from relayr: code={0} ({1})".format(rc, reasons.get(rc, 'unknown')))
+	print "disconnected"
+
+def on_relayrmessage(mosq, userdata, msg):
+	if msg.retain == 1:
+		return
+
+	try:
+		data = json.loads(str(msg.payload))
+	except:
+		return
+
+	readings = data['readings']
+	for reading in readings:
+		xdk = lut[data['deviceId']]
+		new_topic = "%s/%s/%s" % (m.get('prefix'), xdk, reading['path'])
+		new_payload = reading['value']
+		print xdk + " : " + new_topic, " = ", new_payload
+		mqttc.publish(new_topic, json.dumps(new_payload), qos=2, retain=True)
+
+r = cf.config('relayr')
+
+clientid = r.get('client_id', 'sensorimporter-{0}'.format(os.getpid()))
+
+relayr = paho.Client(clientid, clean_session=True, userdata=None, protocol=paho.MQTTv311)
+relayr.on_relayrmessage = on_relayrmessage
+relayr.on_subscribe = on_relayrsubscribe
+relayr.on_connect = on_relayrconnect
+relayr.on_disconnect = on_relayrdisconnect
+
+if r.get('username') is not None:
+	print "pwset %s %s" % (r.get('username'), r.get('password'))
+	relayr.username_pw_set(r.get('username'), r.get('password'))
+
+host = r.get('host', 'localhost')
+port = int(r.get('port', 1883))
+try:
+	print "connecting to %s:%d" % (host, port)
+	relayr.loop_start()
+	relayr.connect(host, port, 60)
+except Exception, e:
+	sys.exit("Connect to `%s:%d': %s" % (host, port, str(e)))
+
 
 while True:
-    try:
-        time.sleep(5)
-    except KeyboardInterrupt:
-        relayr.disconnect()
-        mqttc.disconnect()
-        sys.exit(0)
-    except:
-        raise
+	try:
+		time.sleep(5)
+	except KeyboardInterrupt:
+		relayr.disconnect()
+		mqttc.disconnect()
+		sys.exit(0)
+	except:
+		raise
